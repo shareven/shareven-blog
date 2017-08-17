b39ce99bfe7ea296b49fc051724d88510e05fe1a
shareven <1060776877@qq.com>
2017-08-17 17:49:29 +0800

    完善experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="experience">
-    <div class="piclist">
-      <ul>
-        <li v-for="(item,index) in 6">
-          <img :src="imgUrl[index]" class="img-responsive" alt="">
-        </li>
-      </ul>
-    </div>
-    <div class="dotlist">
-      <ul>
-        <li class="on"></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div class="container">
+      <div id="banner" class="carousel slide col-md-4 col-md-offset-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#banner" data-slide-to="1" class="active"></li>
+          <li data-target="#banner" data-slide-to="2"></li>
+          <li data-target="#banner" data-slide-to="3"></li>
+          <li data-target="#banner" data-slide-to="4"></li>
+          <li data-target="#banner" data-slide-to="5"></li>
+          <li data-target="#banner" data-slide-to="6"></li>
+        </ol>
+  
+        <div class="carousel-inner" role="listbox">
+          <div class="item active" >
+            <img src="static/img/1.jpg" class="img-responsive">
+          </div>
+          <div class="item" v-for="(i,index) in imgUrl">
+            <img :src="i" >
+          </div>
+        </div>
+  
+        <!-- Controls -->
+        <a class="left carousel-control" href="#banner" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#banner" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -26,103 +40,28 @@ export default {
   name: 'experience',
   data() {
     return {
-      imgUrl: ['static/img/1.jpg', 'static/img/2.jpg', 'static/img/3.jpg', 'static/img/4.jpg', 'static/img/5.jpg', 'static/img/6.jpg',]
+      imgUrl: ['static/img/2.jpg', 'static/img/3.jpg', 'static/img/4.jpg', 'static/img/5.jpg', 'static/img/6.jpg',]
     }
   },
   mounted: function () {
-    this.banner();
+    
   },
   methods: {
-    banner() {
-      var $dotlist = $(".dotlist"),
-        $piclistUl = $(".piclist ul"),
-        $piclistLi = $(".piclist li"),
-        width = $piclistLi.width();
-
-      $piclistLi.eq(0).addClass("on");
-      $dotlist.click(function (ev) {
-        $("li.on").removeClass("on");
-        var ev = ev || window.event;
-        ev.target.className = "on";
-        var i = $(ev.target).index();
-        $piclistLi.eq(i).addClass("on");
-        $piclistUl.css("left", -width * i);
-      })
-    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-ul {
-  list-style: none;
+.carousel-indicators li{
+  background: #f92;
+}
+.carousel-indicators li.active{
+  background: #aaa;
 }
 
-.bg(@color) {
-  background: @color;
-}
 
-.box(@w: 100%, @h: 100%) {
-  width: @w;
-  height: @h;
-}
 
-.experience {
-  position: relative;
-  width: 380px;
-  height: 500px;
-  margin: 0 auto;
-  overflow: hidden;
-}
 
-.piclist {
-  position: relative;
-  .box();
-  ul {
-    position: absolute;
-    .box(2100px);
-    transition: .5s ease;
-    li {
-      float: left;
-      .box(380px, 500px);
-    }
-  }
-}
 
-@media and screen (max-width:768px) {
-  .experience {
-    width: 200px;
-    height: 400px;
-  }
-  .piclist {
-    ul {
-      li {
-        .box(200px, 400px);
-      }
-    }
-  }
-}
-
-.dotlist {
-  position: absolute;
-  bottom: 20px;
-  left: 0;
-  right: 0;
-  margin: auto;
-  .box(200px, 15px);
-  ul {
-    display: flex;
-    justify-content: space-around;
-    .box();
-    li {
-      .box(15px);
-      border-radius: 100%;
-      background: #232;
-    }
-    .on {
-      .bg(#eee);
-    }
-  }
-}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,31 @@
 <template>
-  
   <div class="home">
     <div class="container">
       <div class="box">
-        <div class="inner1 in"></div>
-        <div class="inner2 in"></div>
-        <div class="inner3 in"></div>
-        <div class="inner4 in"></div>
-        <div class="inner5 in"></div>
-        <div class="inner6 in"></div>
-        <div class="inner7 in"></div>
-        <div class="inner8 in"></div>
+        <div class="inner1 in">
+          <img src="../assets/img/1.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner2 in">
+          <img src="../assets/img/2.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner3 in">
+          <img src="../assets/img/3.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner4 in">
+          <img src="../assets/img/4.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner5 in">
+          <img src="../assets/img/5.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner6 in">
+          <img src="../assets/img/6.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner7 in">
+          <img src="../assets/img/7.jpg" class="img-responsive" alt="">
+        </div>
+        <div class="inner8 in">
+          <img src="../assets/img/8.jpg" class="img-responsive" alt="">
+        </div>
       </div>
     </div>
   </div>
@@ -22,7 +37,7 @@ export default {
   name: 'home',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      
     }
   }
 }
@@ -30,95 +45,140 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-@media screen and (min-width: 300px) and (max-width: 992px) {
-  .inner1 {
-    margin: 0 auto;
-    width: 100%;
-    height: 600px;
-    background: red url("../assets/img/1.jpg") center center;
+// common
+img {
+  border: none;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+.home {
+  padding-top: 8rem;
+  width: 100%;
+  height: 50rem;
+  font-size: 1rem;
+  background: #000;
+  perspective: 80rem;
+}
+
+.box {
+  position: relative;
+  margin: 0 auto;
+  width: 20rem;
+  height: 20rem;
+  transform-style: preserve-3d;
+  transform: rotateX(80deg) rotateY(0deg) rotateZ(0deg); // transition: 2s;
+  animation: move 40s linear infinite;
+}
+
+@keyframes move {
+  form {
+    transform: rotateX(80deg) rotateY(0deg) rotateZ(0deg);
+  }
+  to {
+    transform: rotateX(80deg) rotateY(0deg) rotateZ(360deg);
   }
 }
 
-@media screen and (min-width: 992px) {
-  html {
-    font-size: 62.5%;
-  }
+.box div.in {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  margin: 0 auto;
+  text-align: center;
+  box-shadow: 0rem 0rem 1rem #f15;
+  width: 10rem; // height: 16rem;
+  line-height: 10rem;
+  background-size: 100%;
+  -webkit-box-reflect: below 0 -webkit-linear-gradient(transparent, transparent 50%, rgba(255, 255, 255, .5));
+}
+
+
+
+/*inner pic*/
+
+.inner1 {
+  transform: translateX(-20rem) rotateX(-90deg) rotateY(-90deg);
+}
+
+.inner2 {
+  transform: translateX(-14.1rem) translateY(-14.1rem) rotateZ(45deg) rotateX(-90deg) rotateY(-90deg);
+}
+
+.inner3 {
+  transform: translateY(-20rem) rotateX(90deg) rotateZ(-180deg);
+}
+
+.inner4 {
+  transform: translateX(14.1rem) translateY(-14.1rem) rotateZ(-45deg) rotateX(-90deg) rotateY(90deg);
+}
+
+.inner5 {
+  transform: translateX(20rem) rotateX(-90deg) rotateY(90deg);
+}
+
+.inner6 {
+  transform: translateX(14.1rem) translateY(14.1rem) rotateZ(-45deg) rotateX(-90deg);
+}
+
+.inner7 {
+  transform: translateY(20rem) rotateX(-90deg);
+}
+
+.inner8 {
+  transform: translateX(-14.1rem) translateY(14.1rem) rotateZ(45deg) rotateX(-90deg);
+}
+
+footer {
+  margin: 5rem auto;
+  font-weight: normal;
+  color: #fff;
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+
+//小屏幕显示
+@media screen and (max-width: 768px) {
   .home {
-    font-size: 1rem;
-    background: #000;
-    perspective: 80rem;
+    padding-top: 20rem;
+    width: 100%;
+    height: 50rem;
   }
   .box {
-    position: relative;
-    margin: 0 auto;
-    width: 50rem;
-    height: 50rem;
-    transform-style: preserve-3d;
-    transform: rotateX(80deg) rotateY(0deg) rotateZ(0deg);
-    transition: 2s;
-    animation: move 40s linear infinite;
-  }
-  @keyframes move {
-    form {
-      transform: rotateX(80deg) rotateY(0deg) rotateZ(0deg);
-    }
-    to {
-      transform: rotateX(80deg) rotateY(0deg) rotateZ(360deg);
-    }
+    width: 10rem;
+    height: 10rem;
   }
   .box div.in {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    margin-left: -5rem;
-    margin-top: -8rem;
-    text-align: center;
-    box-shadow: 0rem 0rem 1rem #f15;
-    width: 10rem;
-    height: 16rem;
-    line-height: 10rem;
-    background-size: 100%;
-    -webkit-box-reflect: below 0 -webkit-linear-gradient(transparent, transparent 50%, rgba(255, 255, 255, .5));
+    width: 6rem; // transition: 2s;
   }
   /*inner pic*/
   .inner1 {
-    background: red url("../assets/img/1.jpg") center center;
-    transform: translateX(-20rem) rotateX(-90deg) rotateY(-90deg);
+    transform: translateX(-8.5rem) rotateX(-90deg) rotateY(-90deg);
   }
   .inner2 {
-    background: blue url("../assets/img/2.jpg") center center;
-    transform: translateX(-14.1rem) translateY(-14.1rem) rotateZ(45deg) rotateX(-90deg) rotateY(-90deg);
+    transform: translateX(-6rem) translateY(-6rem) rotateZ(45deg) rotateX(-90deg) rotateY(-90deg);
   }
   .inner3 {
-    background: green url("../assets/img/3.jpg") center center;
-    transform: translateY(-20rem) rotateX(90deg) rotateZ(-180deg);
+    transform: translateY(-8.5rem) rotateX(90deg) rotateZ(-180deg);
   }
   .inner4 {
-    background: aqua url("../assets/img/4.jpg") center center;
-    transform: translateX(14.1rem) translateY(-14.1rem) rotateZ(-45deg) rotateX(-90deg) rotateY(90deg);
+    transform: translateX(6rem) translateY(-6rem) rotateZ(-45deg) rotateX(-90deg) rotateY(90deg);
   }
   .inner5 {
-    background: pink url("../assets/img/5.jpg") center center;
-    transform: translateX(20rem) rotateX(-90deg) rotateY(90deg);
+    transform: translateX(8.5rem) rotateX(-90deg) rotateY(90deg);
   }
   .inner6 {
-    background: url("../assets/img/6.jpg") center center;
-    transform: translateX(14.1rem) translateY(14.1rem) rotateZ(-45deg) rotateX(-90deg);
+    transform: translateX(6rem) translateY(6rem) rotateZ(-45deg) rotateX(-90deg);
   }
   .inner7 {
-    background: url("../assets/img/7.jpg") center center;
-    transform: translateY(20rem) rotateX(-90deg);
+    transform: translateY(8.5rem) rotateX(-90deg);
   }
   .inner8 {
-    background: url("../assets/img/8.jpg") center center;
-    transform: translateX(-14.1rem) translateY(14.1rem) rotateZ(45deg) rotateX(-90deg);
-  }
-  footer {
-    margin: 5rem auto;
-    font-weight: normal;
-    color: #fff;
-    font-size: 1.2rem;
-    text-align: center;
+    transform: translateX(-6rem) translateY(6rem) rotateZ(45deg) rotateX(-90deg);
   }
 }
 </style>
